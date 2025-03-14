f050d05569baa7c3079bbde7bfeb752ee6f734e2
brian denz <67394688+briandenz@users.noreply.github.com>
2025-03-13 23:43:33 -0400

    Added GET neighborhoods/stores

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get "neighborhoods/index"
   root "maps#index"
-  resources :neighborhoods, only: [:index]
+  resources :neighborhoods, only: [:index] do
+    collection do
+      get 'stores'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
